949f21c01ae69cd06c9db1c133009c5cfed9e621
aruru10313 <taleu3353@gmaill.com>
2026-07-19 15:51:34 +0900

    Fix 1.20.1 entity render signature and TitleScreenMixin warning

--- versions/1.20.1/src/main/java/com/xmaslegacy/menu/screen/CosmeticsScreen.java
+++ versions/1.20.1/src/main/java/com/xmaslegacy/menu/screen/CosmeticsScreen.java
@@ -6,6 +6,7 @@ import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.InventoryScreen;
 import net.minecraft.network.chat.Component;
+import org.joml.Quaternionf;
 
 public class CosmeticsScreen extends Screen {
     private final Screen parent;
@@ -99,7 +100,11 @@ public class CosmeticsScreen extends Screen {
             int scale = 50;
             float lookX = (float) (playerX - mouseX);
             float lookY = (float) (playerY - 50 - mouseY);
-            InventoryScreen.renderEntityInInventory(guiGraphics, playerX, playerY, scale, lookX, lookY, this.minecraft.player);
+            float yaw = (float) Math.atan(lookX / 40.0f);
+            float pitch = (float) Math.atan(lookY / 40.0f);
+            Quaternionf bodyRotation = new Quaternionf().rotateZ((float) Math.PI).rotateY(yaw * 20.0f * 0.017453292f);
+            Quaternionf headRotation = new Quaternionf().rotateX(-pitch * 20.0f * 0.017453292f);
+            InventoryScreen.renderEntityInInventory(guiGraphics, playerX, playerY, scale, bodyRotation, headRotation, this.minecraft.player);
         } else {
             // Draw placeholder text notice
             int textY = containerY + 45;
